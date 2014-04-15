2e9174dc2946906f5c501294bc51bae925606116
korshikov <pk.95@mail.ru>
2014-04-15 12:53:16 +0400

    str_pointer_add

--- hT4_3/src/ExpressionParser.java
+++ hT4_3/src/ExpressionParser.java
@@ -3,12 +3,13 @@
  */
 public class ExpressionParser {
     static private String str;
-
+    static private int pointer=0;
 
     static public Expression3 parse(final String a) {
-        str = a.replaceAll("\\s+", "");
-        str = str.replaceAll("\\s", "");
-        str = str.replaceAll("mod", "%");
+        pointer = 0;
+        str = a.replaceAll("mod", "%").replaceAll("\\s+", "");
+        //str = str.replaceAll("\\s", "");
+        //str = str.replaceAll("mod", "%");
         assert (str.length() > 0);
         return parseAddOrSub();
     }
@@ -16,21 +17,23 @@ public class ExpressionParser {
     static private Expression3 parseAddOrSub() {
         Expression3 left = parseMulDivMod(), right = null;
 
-        while (str.length() > 0) {
-            if ((str.charAt(0) != '+') && (str.charAt(0) != '-')) {
+        while (str.length() > pointer) {
+            if ((str.charAt(pointer) != '+') && (str.charAt(pointer) != '-')) {
                 break;
             }
 
 
-            char sign = str.charAt(0);
+            char sign = str.charAt(pointer);
             switch (sign) {
                 case '+':
-                    str = str.substring(1);
+                    //str = str.substring(1);
+                    pointer++;
                     right = parseMulDivMod();
                     left = new Add(left, right);
                     break;
                 case '-':
-                    str = str.substring(1);
+                    //str = str.substring(1);
+                    pointer++;
                     right = parseMulDivMod();
                     left = new Subtract(left, right);
                     break;
@@ -43,26 +46,29 @@ public class ExpressionParser {
     static private Expression3 parseMulDivMod() {
         Expression3 left = parseBrackets(), right = null;
 
-        while (str.length() > 0) {
-            if ((str.charAt(0) != '*') && (str.charAt(0) != '/') && (str.charAt(0) != '%')) {
+        while (str.length() > pointer) {
+            if ((str.charAt(pointer) != '*') && (str.charAt(pointer) != '/') && (str.charAt(pointer) != '%')) {
                 break;
             }
 
 
-            char sign = str.charAt(0);
+            char sign = str.charAt(pointer);
             switch (sign) {
                 case '*':
-                    str = str.substring(1);
+                    //str = str.substring(1);
+                    pointer++;
                     right = parseBrackets();
                     left = new Multiply(left, right);
                     break;
                 case '/':
-                    str = str.substring(1);
+                    //str = str.substring(1);
+                    pointer++;
                     right = parseBrackets();
                     left = new Divide(left, right);
                     break;
                 case '%':
-                    str = str.substring(1);
+                    //str = str.substring(1);
+                    pointer++;
                     right = parseBrackets();
                     left = new Mod(left, right);
                     break;
@@ -74,10 +80,12 @@ public class ExpressionParser {
 
 
     static private Expression3 parseBrackets() {
-        if (str.charAt(0) == '(') {
-            str = str.substring(1);
+        if ((str.length()>pointer)&&(str.charAt(pointer) == '(')) {
+            //str = str.substring(1);
+            pointer++;
             Expression3 ret = parseAddOrSub();
-            str = str.substring(1);
+            //str = str.substring(1);
+            pointer++;
             return ret;
         } else {
             return parseUnaryOperation();
@@ -85,18 +93,20 @@ public class ExpressionParser {
     }
 
     static private Expression3 parseUnaryOperation() {
-        char tmp = str.charAt(0);
+        char tmp = str.charAt(pointer);
         Expression3 inside;
         switch (tmp) {
             case '~':
-                str = str.substring(1);
+                //str = str.substring(1);
+                pointer++;
                 inside = parseUnaryOperation();
                 return new Neg(inside);
             case '-':
-                if ((str.charAt(1) >= '0') && (str.charAt(1) <= '9') || (str.charAt(1) == '.')) {
+                if ((str.charAt(pointer+1) >= '0') && (str.charAt(pointer+1) <= '9') || (str.charAt(pointer+1) == '.')) {
                     return parseNum();
                 }
-                str = str.substring(1);
+                //str = str.substring(1);
+                pointer++;
                 inside = parseUnaryOperation();
                 return new UnarySubtract(inside);
         }
@@ -104,20 +114,22 @@ public class ExpressionParser {
     }
 
     static private Expression3 parseNum() {
-        if ((str.length() > 0) && (str.charAt(0) == '(')) {
+        if ((str.length() > pointer) && (str.charAt(pointer) == '(')) {
             return parseBrackets();
         }
         Expression3 ret = null;
-        if ((str.length() > 0) && ((str.charAt(0) >= '0') && (str.charAt(0) <= '9') || (str.charAt(0) == '.') || (str.charAt(0) == '-'))) {
+        if ((str.length() > pointer) && ((str.charAt(pointer) >= '0') && (str.charAt(pointer) <= '9') || (str.charAt(pointer) == '.') || (str.charAt(pointer) == '-'))) {
             int numLen = 1;
-            while ((str.length() > numLen) && ((str.charAt(numLen) >= '0') && (str.charAt(numLen) <= '9') || (str.charAt(numLen) == '.'))) {
+            while ((str.length() > numLen+pointer) && ((str.charAt(numLen+pointer) >= '0') && (str.charAt(numLen+pointer) <= '9') || (str.charAt(numLen+pointer) == '.'))) {
                 numLen++;
             }
-            ret = new Const(Integer.parseInt(str.substring(0, numLen)));
-            str = str.substring(numLen);
+            ret = new Const(Integer.parseInt(str.substring(pointer,pointer+ numLen)));
+            //str = str.substring(numLen);
+            pointer+=numLen;
         } else {
-            ret = new Variable(str.substring(0, 1));
-            str = str.substring(1);
+            ret = new Variable(str.substring(pointer, pointer+1));
+            //str = str.substring(1);
+            pointer++;
         }
         return ret;
     }
